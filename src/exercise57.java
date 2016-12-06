9d852c98411e7c49fc0cf10592c2b7498435b118
prasmus <rasmus.prees@tptlive.ee>
2016-12-06 13:43:20 +0200

    parandused

--- src/exercise57.java
+++ src/exercise57.java
@@ -1,30 +1,21 @@
 /**
  * Created by Praktika on 11.11.2016.
  */
-import java.util.Random;
-import java.util.Scanner;
-
-public class exercise57 {
+public class exercise 67 {
     public static void main(String[] args) {
-        System.out.println("I'm thinking of a number between 1 and 100 (including both). Can you guess what it is?");
-        Random random = new Random();
-        int myguess = random.nextInt(100) + 1;
-        methodman(myguess);
-    }
-    public static void methodman(int myguess) {
-        System.out.print("Type a number: ");
-        Scanner in = new Scanner(System.in);
-        int yourguess = in.nextInt();
-
-        if (myguess < yourguess) {
-            System.out.println("Your guess is too high");
-            methodman(myguess);
-        } else if (myguess > yourguess){
-            System.out.println("Your guess is too low");
-            methodman(myguess);
+        int n = 5;
+        if(n % 2 == 0) {
+            System.out.println("Sisesta ainult paaritu arv");
         } else {
-            System.out.print("Your guess is on point! Gratz!");
+            oddSum(n);
         }
-
+        System.out.println(oddSum(n));
+    }
+    public static int oddSum(int n) {
+        if(n <= 0) {
+            return 0;
+        } else {
+            return(n + oddSum(n - 2));
+        }
     }
 }
